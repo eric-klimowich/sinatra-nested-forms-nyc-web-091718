f8c76091ae66cd274f086fc4ccc45af56fe47ebd
eric-klimowich <eklimowich@hotmail.com>
2018-10-10 15:42:56 -0400

    Pirate booty

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships = []
+  end
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@ships << self
+  end
+
 end
